aaed7347d0f0b099a82f55e0c8eb25aeaffb0ac1
jgmurua <jgmurua@gmail.com>
2022-05-14 21:19:42 -0300

    Update k3s.sh
     --disable traefik  --disable servicelb options to disable  ingress controler

--- infra/k3s/k3s.sh
+++ infra/k3s/k3s.sh
@@ -3,5 +3,6 @@
 sudo /usr/local/bin/k3s-uninstall.sh
 ip=$(curl http://checkip.amazonaws.com)
 curl -sfL https://get.k3s.io | sh -s - server --tls-san $ip
+#curl -sfL https://get.k3s.io | sh -s - server --tls-san $ip  --disable traefik  --disable servicelb
 sudo k3s kubectl get node
 sudo sed "s/127.0.0.1/$ip/g" /etc/rancher/k3s/k3s.yaml
